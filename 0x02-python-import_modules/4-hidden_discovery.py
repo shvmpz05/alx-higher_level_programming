e4a30bce0023a9d07c0b563f423414d21aafb043
shvmpz05 <martinkimani914@gmail.com>
2023-01-06 10:49:44 +0000

    Some stuff

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    content = dir(hidden_4)
+    for i in content:
+        if i[0] == '_':
+            continue
+        else:
+            print("{}".format(i))
