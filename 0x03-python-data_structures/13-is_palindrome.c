315346ff176e3ef5f9a938895bea60ce2195d1fb
shvmpz05 <martinkimani914@gmail.com>
2023-01-08 15:21:34 +0000

    Some stuff

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,59 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list
- *
- * Return: 0 if the list is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - checks for list is palindrome
+ * @head: head of linked list
+ * Return:  boolean
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *temp, *front, *back;
-    int values[1024];
-    int i, j;
+	int len = 0, i = 0;
+	listint_t *tmp;
+	int Ns[10000];
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    front = *head;
-    back = *head;
-    i = 0;
-
-    while (front->next != NULL)
-    {
-        values[i] = front->n;
-        i++;
-        front = front->next;
-        if (front->next != NULL)
-            front = front->next;
-        else
-            break;
-        back = back->next;
-    }
-
-    if (front->next == NULL && i % 2 == 0)
-    {
-        values[i] = front->n;
-        i++;
-    }
-
-    temp = back->next;
-    back->next = NULL;
-    back = temp;
-
-    j = i - 1;
-    for (i = 0; i < j; i++, j--)
-    {
-        if (values[i] > 0 && values[j] < 0)
-            return (0);
-        if (values[i] < 0 && values[j] > 0)
-            return (0);
-        if (values[i] != values[j])
-            return (0);
-    }
-
-    return (1);
+	tmp = *head;
+	if ((*head) == NULL)
+		return (1);
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	if (len == 1)
+		return (1);
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		Ns[i] = tmp->n;
+		tmp = tmp->next;
+		i++;
+	}
+	for (i = 0; i <= len / 2; i++)
+	{
+		if (Ns[i] != Ns[len - i - 1])
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
-
