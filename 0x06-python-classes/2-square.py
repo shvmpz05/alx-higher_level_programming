94e8383db9083ec03d92c406ccfdcab75def852c
shvmpz05 <martinkimani914@gmail.com>
2023-01-24 14:58:40 +0000

    Some Stuff

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
 """
 This module contains class Sqaure
-
 """
+
+
 class Square:
 
-    """
-    Initialize an instace of Square
+    """Initialize an instace of Square"""
 
-    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif (size < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    """
-    Check if the size of the square is less than zero
-    or Not an integer
-
-    __size(int) = the size of the square
-
-    """
+        self.__size = size
