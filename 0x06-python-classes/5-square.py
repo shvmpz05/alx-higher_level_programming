893937e9b9c344fd60da8d9b43d773c6129598ab
shvmpz05 <martinkimani914@gmail.com>
2023-01-24 16:27:17 +0000

    Some Stuff

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" This module will create a class Square and allow to get the area """
+
+
+class Square:
+
+    """Initialize a sqaure and the dimensions"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    def area(self):
+        return self.__size ** 2
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("{}".format("#"), end="")
+                print()
