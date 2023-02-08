0d39ca5193ef4721fb606f980089029b00586eae
shvmpz05 <martinkimani914@gmail.com>
2023-02-08 10:24:02 +0000

    more comments

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,8 @@
 
 
 def append_write(filename="", text=""):
+    """Return number of character written"""
+
     with open(filename, "a", encoding="UTF8") as file:
         mychar = file.write(text)
     return mychar
